f58618eec331067190c357f9c6bb47c9bcf4fabb
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-13 10:29:16 +0100

    jQuery module

---
+++ 03 - jQuery/jQuery/Demo.js
@@ -0,0 +1,38 @@
+﻿
+$(function () {
+
+    //$("li").css("color", "green");
+
+
+    $("#root > li:gt(3)").css("color", "green");
+
+    $("ul> ul > li")
+        .css('background-color', 'red')
+        .text('Demo')
+        .css('color', 'yellow');
+
+
+    $("<li>").text("new element").appendTo("#root");
+
+
+    //$("#btn").click(function () {
+
+    //    //$("#root").slideToggle(5000);
+    //    $("#root").toggleClass("demo");
+    //});
+
+    $("#btn").bind('click', function () {
+        console.log("button clicked");
+        return false;
+    });
+
+    $("body").bind('click', function () {
+        console.log("body clicked");
+    });
+
+    $("ul").css("display", "block");
+
+    $("#load").click(function () {
+        $("#placeholder").load("/NestedPage.html p:eq(1)");
+    });
+});
