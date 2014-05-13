572f95148f4bb7715a481b88cecf5e99ad5b9b09
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-13 13:45:46 +0100

    jQuery UI

---
+++ 04 - jQuery UI/jQueryUI/Demo.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+
+    //$(".large").click(function () {
+    //    //$(this).effect('puff');
+    //    $(this).switchClass("large", "xlarge", 5000, "easeInOutQuad");
+    //});
+
+
+    //$(".small").draggable();
+
+    //$(".large").droppable({
+    //    drop: function() {
+    //        console.log("Something was dropped");
+    //    }
+    //});
+
+    $(".large").resizable();
+
+
+
+    $.fn.makeYellow = function () {
+        this.css('background-color', 'yellow');
+
+        $("<p>").text("new data").appendTo(this);
+
+        return this;
+    };
+
+    $(".large").makeYellow();
+});
