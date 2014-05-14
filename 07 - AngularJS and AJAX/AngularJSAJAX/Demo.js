8e1a216202e34c02b4be0b94058c3bb51487aca1
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-14 14:06:20 +0100

    07 - AngularJS and AJAX

---
+++ 07 - AngularJS and AJAX/AngularJSAJAX/Demo.js
@@ -0,0 +1,40 @@
+﻿
+var app = angular.module("myApp", ["ngResource"]);
+
+app.factory("Books", function ($resource) {
+    return $resource("/api/books/:id");
+});
+
+//app.controller("booksCtrl", function ($scope, $http) {
+//    $scope.books = [
+//    ];
+
+
+//    $http.get("/api/books").then(function (e) {
+//        console.log(e);
+
+//        $scope.books = e.data;
+//    });
+
+
+//    $scope.addBook = function() {
+//        var newBook = { title: "The new book", author:"Me" };
+//        $http.post("/api/books", newBook);
+
+
+//    }
+//});
+
+
+app.controller("booksCtrl", function ($scope, Books) {
+    $scope.books = Books.query();
+    console.log($scope.books.length);
+    $scope.books.$promise.then(function() {
+        console.log($scope.books.length);
+    });
+
+    $scope.addBook = function () {
+        var newBook = { title: "The new book", author: "Me" };
+        Books.save(newBook);
+    };
+});
