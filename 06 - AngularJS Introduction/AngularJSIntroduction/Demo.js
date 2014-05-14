c3bf388cabbfb282b8ea1c0841daa7372125571d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-14 12:57:54 +0100

    06 - AngularJS Introduction

---
+++ 06 - AngularJS Introduction/AngularJSIntroduction/Demo.js
@@ -0,0 +1,38 @@
+﻿var other = angular.module("otherModule", []);
+
+other.service("mySvc", function () {
+});
+
+
+
+
+var module = angular.module("myApp", ["otherModule"]);
+
+
+module.controller("MyFirstController", ["$scope", "mySvc", function ($scope, mySvc) {
+
+    console.log("MyFirstController");
+
+    var model = {
+        name: "Maurice",
+        books: [
+            { title: "Book 1" },
+            { title: "Book 2" },
+            { title: "Book 3" },
+            { title: "Book 4" }
+        ],
+        now: new Date(),
+        number: 1.23
+    };
+
+    $scope.model = model;
+
+    $scope.showModel = function () {
+        alert(model.name);
+    };
+
+    $scope.addBook = function () {
+        model.books.push({ title: "A new book" });
+    };
+}]);
+
---
+++ 06 - AngularJS Introduction/AngularJSIntroduction/SPA.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/list", {
+        controller: "ListCtrl",
+        template: "The list <button ng-click='go()'>CLick me </button>"
+    });
+
+    $routeProvider.when("/details", {
+        controller: "DetailsCtrl",
+        template: "The details"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/list" });
+});
+
+
+app.controller("ListCtrl", function($scope, $location) {
+
+    $scope.go = function() {
+        $location.path("/details");
+    };
+})
+
+app.controller("DetailsCtrl", function () {
+
+  
+})
+
