c4d3c20ccf573d19c0ce61f9e6262483b50fca53
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-14 16:01:27 +0100

    08 - AngularJS Directives

---
+++ 08 - AngularJS Directives/AngularJSDirectives/Demo.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("theApp", []);
+
+app.controller("mainCtrl", function ($scope) {
+    $scope.msg = "Hello";
+});
+
+app.directive("rbsAlertOnClick", function ($http) {
+    var ddo = {
+        restrict: "AE",
+        scope: {
+            myMsg: "=",
+            myNow: "="
+        },
+        require:"ngModel",
+        link: function(scope, element, attrs, ngModel) {
+            element.bind("click", function () {
+                scope.$apply(function() {
+                    scope.myNow = new Date().toLocaleTimeString();
+
+                    ngModel.$setViewValue("");
+                });
+                alert("I was clicked from " + scope.$id + " " + scope.myMsg);
+            });
+        }
+
+    };
+
+    return ddo;
+});
