883e1b242d25329032a48299c904aeab20a81360
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-15 14:19:27 +0100

    10 - REST

---
+++ 10 - REST/REST/Api/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using RestDemo.Models;
+
+namespace REST.Api
+{
+    public class BooksController: ApiController
+    {
+        private IBooksRepository _repo = new BooksRepository();
+        
+        //[HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+           return _repo.GetBooks();
+        }
+
+        public HttpResponseMessage GetBook(int id)
+        {
+            var book = _repo.GetBook(id);
+            if (book == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, book);
+        }
+    }
+}
