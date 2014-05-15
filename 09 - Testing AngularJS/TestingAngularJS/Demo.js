cc68669cff003b1a0e0154bc2a031cd408d88989
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-05-15 10:56:41 +0100

    09 - Testing AngularJS

---
+++ 09 - Testing AngularJS/TestingAngularJS/Demo.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("theApp", []);
+
+app.controller("mainCtrl", function ($scope) {
+    $scope.msg = "Hello";
+});
+
+app.controller("ajaxCtrl", function ($scope, $http) {
+    $scope.msg = "Loading";
+
+    $http.get("/api/message/1").then(function(e) {
+        $scope.msg = e.data;
+    }, function() {
+        $scope.msg = "Oops";
+    });
+});
+
+
+app.directive("myDirective", function() {
+    return {
+        link: function(scope, element) {
+            element.bind("click", function() {
+                scope.$apply(function() {
+                    scope.msg = "I was clicked";
+                });
+            });
+        }
+    };
+});
+
---
+++ 09 - Testing AngularJS/TestingAngularJS/DemoSpecs.js
@@ -0,0 +1,49 @@
+﻿describe("Our app", function () {
+
+    it("should pass", function () {
+
+        expect(1).toBe(1);
+    });
+
+});
+
+
+describe("TheApp", function () {
+    var scope;
+
+    beforeEach(module("theApp"));
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        var ctrl = $controller("mainCtrl", { $scope: scope });
+    }));
+
+    it("should add a message to the scope", function () {
+        expect(scope.msg).toBe("Hello");
+    });
+})
+
+
+describe("TheApp with AJAX", function () {
+    var scope, httpBackend;
+
+    beforeEach(module("theApp"));
+
+    beforeEach(inject(function ($rootScope, $controller, $httpBackend) {
+        httpBackend = $httpBackend;
+        scope = $rootScope.$new();
+        var ctrl = $controller("ajaxCtrl", { $scope: scope });
+    }));
+
+    it("should add a message to the scope", function () {
+        httpBackend.expect("GET", "/api/message/1").respond("AJAX result");
+        httpBackend.flush();
+        expect(scope.msg).toBe("AJAX result");
+    });
+
+    it("should add a fail message to the scope", function () {
+        httpBackend.expect("GET", "/api/message/1").respond(500);
+        httpBackend.flush();
+        expect(scope.msg).toBe("Oops");
+    });
+})
